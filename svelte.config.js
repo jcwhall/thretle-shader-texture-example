0ab4533d75398ae58d185001a385ab140382e2c5
teamtedproductions <47797947+teamtedproductions@users.noreply.github.com>
2025-12-15 22:54:32 +0100

    Docs

--- svelte.config.js
+++ svelte.config.js
@@ -12,12 +12,15 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
+			pages: 'docs',
+			assets: 'docs',
 			fallback: '404.html',
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: '/thretle-shader-texture-example'
+		}
 	}
 };
 
